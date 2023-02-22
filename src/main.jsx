4ae1d5ea239ac1bac55d25e9fbde02fd44807797
Lestrades <lestrades@gmail.com>
2023-02-22 17:53:11 +0100

    Lecc-163

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index, { loader as clientesLoader } from './pages/Index'
+
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Inicio</h1>
-  }
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: clientesLoader,
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />
+      }
+    ]
+  },
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
